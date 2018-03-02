8dea53327336aa312f5844bb263ba284b2ae2529
Fabian Müller <fmueller@mpi-inf.mpg.de>
2018-03-02 14:16:42 -0800

    more TF motif sets supported using chromVARmotifs

--- R/utils.R
+++ R/utils.R
@@ -29,8 +29,8 @@ isCanonicalChrom <- function(ss){
 #' 
 #' prepare objects for a \code{motifmatchr} analysis
 #' @param genome character string specifying genome assembly
-#' @param motifs either a character string (currently only "jaspar" is supported) or an object containing PWMs
-#'               that can be used by \code{motifmatchr::matchMotifs} (such as an \code{PFMatrixList} object)
+#' @param motifs either a character string (currently only "jaspar" and sets contained in \code{chromVARmotifs} ("homer", "encode", "cisbp") are supported) or an object containing PWMs
+#'               that can be used by \code{motifmatchr::matchMotifs} (such as an \code{PFMatrixList} or \code{PWMatrixList} object)
 #' @return a list containing objects to be used as arguments for \code{motifmatchr}
 #' @author Fabian Mueller
 #' @noRd
@@ -56,15 +56,43 @@ prepareMotifmatchr <- function(genome, motifs){
 	}
 
 	# get the motif PWMs
+	motifL <- TFBSTools::PWMatrixList()
 	if (is.character(motifs)){
-		if (motifs=="jaspar"){
+		if (is.element("jaspar", motifs)){
 			require(chromVAR)
-			motifs <- getJasparMotifs(species=spec)
-		} else {
-			logger.error(c("Unsupported motifs:", motifs))
+			motifL <- c(motifL, getJasparMotifs(species=spec))
 		}
-	}
+		if (is.element("homer", motifs)){
+			require(chromVARmotifs)
+			data("homer_pwms")
+			motifL <- c(motifL, homer_pwms)
+		}
+		if (is.element("encode", motifs)){
+			require(chromVARmotifs)
+			data("encode_pwms")
+			motifL <- c(motifL, encode_pwms)
+		}
+		if (is.element("cisbp", motifs)){
+			require(chromVARmotifs)
+			if (spec == "Mus musculus"){
+				data("mouse_pwms_v2")
+				motifL <- c(motifL, mouse_pwms_v2)
+			} else if (spec == "Homo sapiens"){
+				data("human_pwms_v2")
+				motifL <- c(motifL, human_pwms_v2)
+			} else {
+				logger.warning(c("Could not find cisBP annotation for species", spec))
+			}
+		} 
+		if (length(motifL) < 1) {
+			logger.error(c("No motifs were loaded. Unsupported motifs (?) :", motifs))
+		}	
+	} else if (is.element("PWMatrixList", class(motifs)) || is.element("PFMatrixList", class(motifs))) {
+		motifL <- motifs
+	} else {
+		logger.error(c("unsupported value for motifs:", motifs))
+	}	
 	res[["genome"]] <- genomeObj
-	res[["motifs"]] <- motifs
+	res[["motifs"]] <- motifL
 	return(res)
 }
