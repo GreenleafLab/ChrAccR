4eef5951514efc1001e8fc306a7c960c908854f7
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-01-20 14:11:59 -0800

    minor bug fix (tf-idf check)

--- R/DsATAC-class.R
+++ R/DsATAC-class.R
@@ -1063,7 +1063,7 @@ setMethod("transformCounts",
 		if (!all(regionTypes %in% getRegionTypes(.object))){
 			logger.error(c("Unsupported region type:", paste(setdiff(regionTypes, getRegionTypes(.object)), collapse=", ")))
 		}
-		if (!is.element(method, c("quantile", "log2", "RPKM", "vst"))) logger.error(c("Unsupported normalization method type:", method))
+		if (!is.element(method, c("quantile", "log2", "RPKM", "vst", "tf-idf"))) logger.error(c("Unsupported normalization method type:", method))
 
 		if (method == "quantile"){
 			logger.start(c("Performing quantile normalization"))
