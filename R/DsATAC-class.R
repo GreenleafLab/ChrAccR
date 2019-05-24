ef1455c717b8a978391edd06914dda3980c68c7f
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-05-24 10:26:48 -0700

    …

--- R/DsATAC-class.R
+++ R/DsATAC-class.R
@@ -2467,6 +2467,7 @@ if (!isGeneric("unsupervisedAnalysisSc")) {
 #'
 #' @param .object    \code{\linkS4class{DsATAC}} object
 #' @param regionType character string specifying the region type
+#' @param regionIdx  indices of regions to be used (logical or integer vector). If \code{NULL} (default) all regions of the specified regionType will be used.
 #' @param dimRedMethod character string specifying the dimensionality reduction method. Currently on \code{"tf-idf_irlba"} is supported
 #' @param usePcs     integer vector specifying the principal components to use for UMAP and clustering
 #' @param clusteringMethod character string specifying the clustering method. Currently on \code{"seurat_louvain"} is supported
@@ -2485,45 +2486,64 @@ setMethod("unsupervisedAnalysisSc",
 	function(
 		.object,
 		regionType,
+		regionIdx=NULL,
 		dimRedMethod="tf-idf_irlba",
 		usePcs=1:50,
 		clusteringMethod="seurat_louvain"
 	) {
 		if (!is.element(regionType, getRegionTypes(.object))) logger.error(c("Unsupported region type:", regionType))
-		if (!is.element(dimRedMethod, c("tf-idf_irlba")) logger.error(c("Unsupported dimRedMethod:", dimRedMethod))
+		if (!is.element(dimRedMethod, c("tf-idf_irlba"))) logger.error(c("Unsupported dimRedMethod:", dimRedMethod))
 		if (!is.integer(usePcs)) logger.error(c("usePcs must be an integer vector"))
-		if (!is.element(clusteringMethod, c("seurat_louvain")) logger.error(c("Unsupported clusteringMethod:", clusteringMethod))
+		if (!is.element(clusteringMethod, c("seurat_louvain"))) logger.error(c("Unsupported clusteringMethod:", clusteringMethod))
+		if (!is.null(regionIdx)){
+			if (is.logical(regionIdx)) regionIdx <- which(regionIdx)
+			if (!is.integer(regionIdx) || any(regionIdx < 1) || any(regionIdx > getNRegions(.object, regionType))) logger.error("Invalid regionIdx")
+		}
 
+		dsn <- .object
+		if (!is.null(regionIdx)){
+			nRegs <- getNRegions(.object, regionType)
+			logger.info(c("Retaining", length(regionIdx), "regions for dimensionality reduction"))
+			idx <- rep(TRUE,  nRegs)
+			idx[regionIdx] <- FALSE
+			dsn <- removeRegions(.object, idx, regionType)
+		}
 		if (dimRedMethod=="tf-idf_irlba"){
-			if (length(.object@countTransform[[regionType]]) > 0) logger.warning("Counts have been pre-normalized. dimRedMethod 'tf-idf_irlba' might not be applicable.")
-			dsn <- .object
-			if (!is.element("tf-idf", .object@countTransform[[regionType]])){
-				dsn <- transformCounts(.object, method="tf-idf", regionTypes=regionType)
-			}
-			cm <- ChrAccR::getCounts(dsn, regionType, asMatrix=TRUE)
-			pcaCoord <- muRtools::getDimRedCoords.pca(t(cm), components=1:max(pcs), method="irlba_svd")
+			logger.start(c("Performing dimensionality reduction using", dimRedMethod))			
+				if (length(dsn@countTransform[[regionType]]) > 0) logger.warning("Counts have been pre-normalized. dimRedMethod 'tf-idf_irlba' might not be applicable.")
+				
+				if (!is.element("tf-idf", dsn@countTransform[[regionType]])){
+					dsn <- transformCounts(dsn, method="tf-idf", regionTypes=regionType)
+				}
+
+				cm <- ChrAccR::getCounts(dsn, regionType, asMatrix=TRUE)
+				pcaCoord <- muRtools::getDimRedCoords.pca(t(cm), components=1:max(pcs), method="irlba_svd")
+			logger.completed()
 		}
 		cellIds <- colnames(cm)
-		umapCoord <- muRtools::getDimRedCoords.umap(pcaCoord[,usePcs])
+		logger.start(c("Getting UMAP coordinates"))	
+			umapCoord <- muRtools::getDimRedCoords.umap(pcaCoord[,usePcs])
+		logger.completed()
 
 		if (clusteringMethod=="seurat_louvain"){
-			require(Seurat)
-			# Louvain clustering using Seurat
-			dummyMat <- matrix(11.0, ncol=length(cellIds), nrow=11)
-			colnames(dummyMat) <- cellIds
-			sObj <- CreateSeuratObject(dummyMat, project='scATAC', min.cells=0, min.genes=0)
-			sObj <- SetDimReduction(object=sObj, reduction.type="pca", slot="cell.embeddings", new.data=pcaCoord)
-			sObj <- SetDimReduction(object=sObj, reduction.type="pca", slot="key", new.data="pca")
-			clustRes <- FindClusters(sObj, reduction.type="pca", dims.use=usePcs, k.param=30, algorithm=1, n.start=100, n.iter=10)
-			clustAss <- clustRes@ident
+			logger.start(c("Performing clustering using", clusteringMethod))	
+				require(Seurat)
+				# Louvain clustering using Seurat
+				dummyMat <- matrix(11.0, ncol=length(cellIds), nrow=11)
+				colnames(dummyMat) <- cellIds
+				sObj <- CreateSeuratObject(dummyMat, project='scATAC', min.cells=0, min.genes=0)
+				sObj <- SetDimReduction(object=sObj, reduction.type="pca", slot="cell.embeddings", new.data=pcaCoord)
+				sObj <- SetDimReduction(object=sObj, reduction.type="pca", slot="key", new.data="pca")
+				clustRes <- FindClusters(sObj, reduction.type="pca", dims.use=usePcs, k.param=30, algorithm=1, n.start=100, n.iter=10)
+				clustAss <- clustRes@ident
+			logger.completed()
 		}
-
-		logger.start("Saving environment")
 		res <- list(
 			pcaCoord=pcaCoord,
 			umapCoord=umapCoord,
 			clustAss=clustAss,
-			regionType=regionType
+			regionType=regionType,
+			regionIdx=regionIdx
 		)
 		class(res) <- "unsupervisedAnalysisResultSc"
 		return(res)
