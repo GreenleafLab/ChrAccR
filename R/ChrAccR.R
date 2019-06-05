a5f771ddccb63b133de3ae5d7f5bc5095a309801
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-06-05 14:57:26 -0700

    imports

---
+++ R/ChrAccR.R
@@ -0,0 +1,14 @@
+#' ChrAccR: Analyzing chromatin accessibility data in R.
+#'
+#' Tools for analyzing chromatin accessibility data in R. Currently supports ATAC-seq and NOMe-seq data analysis.
+#'
+#' @import Matrix
+#' @import data.table
+#' @import GenomicRanges
+#' @import GenomicAlignments
+#' @import ggplot2
+#' @import muLogR
+#' @import muRtools
+#' @docType package
+#' @name ChrAccR
+NULL
