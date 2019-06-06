926817db287a3f038ac1b2c3d3eec4e7abf88ebd
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-06-06 10:17:05 -0700

    …

--- R/ChrAccR.R
+++ R/ChrAccR.R
@@ -11,7 +11,7 @@
 #' @importFrom IRanges IRanges overlapsAny
 #' @importFrom S4Vectors queryHits subjectHits queryLength DataFrame elementNROWS
 #' @import GenomicAlignments
-#' @importFrom GenomeInfoDb seqlevels seqlevels<- seqlengths seqnames genome genome<-
+#' @importFrom GenomeInfoDb seqlevels seqlevels<- seqlengths seqnames genome genome<- organism
 #' @import SummarizedExperiment
 #' @import ggplot2
 #' @import muLogR
