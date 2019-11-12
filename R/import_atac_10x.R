2cc9f35f5eaaed7287c7ecb9669b19e12ca91df8
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-11-12 13:16:13 -0800

    added DsATACsc.fragments for parsing scATAC data

--- R/import_atac_10x.R
+++ R/import_atac_10x.R
@@ -1,4 +1,141 @@
 #-------------------------------------------------------------------------------
+#' DsATACsc.fragments
+#' 
+#' Create a DsATACsc dataset from multiple input fragment files
+#' @param sampleAnnot  data.frame specifying the sample annotation table
+#' @param fragmentFiles vector of fragment files or the column name in the sample annotation table containing thse file names.
+#'                     fragment files must be tab-separated with columns "chrom", "chromStart", "chromEnd", "barcode" and "duplicateCount"
+#'                     and must not contain a header line
+#' @param genome       genome assembly
+#' @param regionSets   a list of GRanges objects which contain region sets over which count data will be aggregated
+#' @param sampleIdCol  column name or index in the sample annotation table containing unique sample identifiers
+#' @param cellAnnot    (optional) annotation table of all cells in the dataset. Must contain a \code{'cellId'} column.
+#' @param diskDump     should large data objects (count matrices, fragment data, ...) be disk-backed to save main memory
+#' @param keepInsertionInfo flag indicating whether to maintain the insertion information in the resulting object.
+#' @return \code{\linkS4class{DsATACsc}} object
+#' @author Fabian Mueller
+#' @export
+DsATACsc.fragments <- function(sampleAnnot, fragmentFiles, genome, regionSets=NULL, sampleIdCol=NULL, cellAnnot=NULL, diskDump=FALSE, keepInsertionInfo=FALSE){
+	if (!is.character(fragmentFiles)) logger.error("Invalid value for fragmentFiles. Expected character")
+	if (length(fragmentFiles)==1 && is.element(fragmentFiles, colnames(sampleAnnot))){
+		fragmentFiles <- sampleAnnot[,fragmentFiles]
+	} else {
+		if (length(fragmentFiles)!=nrow(sampleAnnot)) logger.error("Invalid value for fragmentFiles. must match the number of rows in sampleAnnot")
+	}
+	if (!is.null(sampleIdCol) && !(is.character(sampleIdCol) && length(sampleIdCol)==1)) logger.error("Invalid value for sampleIdCol Expected character or NULL")
+	if (is.null(sampleIdCol)){
+		sampleIdCol <- grep("sample", colnames(sampleAnnot), value=TRUE)
+		if (length(sampleIdCol) != 1) logger.error("Could not uniquely determine sample id column from sample annotation column names")
+		logger.info(c("Automatically determined column '", sampleIdCol, "' as sample identifier column"))
+	}
+	sampleIds <- sampleAnnot[,sampleIdCol]
+
+	if (is.null(regionSets)){
+		logger.info(c("Using default region sets:", paste("tiling200bp", collapse=", ")))
+		regionSets <- GRangesList(
+			tiling200bp=getTilingRegions(genome, width=200L, onlyMainChrs=TRUE)
+		)
+	}
+	
+	if (is.null(cellAnnot)){
+		cellAnnot <- do.call("rbind", lapply(1:nrow(sampleAnnot), FUN=function(i){
+			sid <- sampleIds[i]
+			fragTab <- readTab(fragmentFiles[i], header=FALSE)
+			colnames(fragTab) <- c("chrom", "chromStart", "chromEnd", "barcode", "duplicateCount")
+			fragCounts <- table(fragTab[,"barcode"])
+			sa.sample <- sampleAnnot[sid,]
+			rownames(sa.sample) <- NULL
+			sa <- data.frame(
+				.sampleId=sid,
+				cellId=paste(sid, names(fragCounts), sep="_"),
+				barcode=names(fragCounts),
+				sa.sample,
+				nFrags=fragCounts,
+				stringsAsFactors=FALSE
+			)
+			return(sa)
+		}))
+	} else {
+		if (!is.element("cellId", colnames(cellAnnot))){
+			logger.error("Invalid parameter: cellAnnot. Expected table with column 'cellId'")
+		}
+	}
+	logger.start("Creating DsATAC object")
+		obj <- DsATACsc(cellAnnot, genome, diskDump=diskDump, diskDump.fragments=keepInsertionInfo, sparseCounts=TRUE)
+		for (rt in names(regionSets)){
+			logger.info(c("Including region set:", rt))
+			obj <- regionAggregation(obj, regionSets[[rt]], rt, signal=NULL, dropEmpty=FALSE)
+		}
+		if (obj@diskDump.fragments && .hasSlot(obj, "diskDump.fragments.nSamplesPerFile")){
+			obj@diskDump.fragments.nSamplesPerFile <- 500L
+		}
+	logger.completed()
+
+	logger.start("Reading scATAC data (fragment data)")
+		nSamples <- length(sampleIds)
+		for (i in seq_along(fragmentFiles)){
+			sid <- sampleIds[i]
+			logger.start(c("Importing sample", ":", sid, paste0("(", i, " of ", nSamples, ")")))
+				sampleIdx <- cellAnnot[,sampleIdCol]==sid
+				barcode2cellId <- cellAnnot[sampleIdx,"cellId"]
+				names(barcode2cellId) <- cellAnnot[sampleIdx,".CR.cellQC.barcode"]
+
+				logger.start("Preparing fragment data")
+					fragGr <- readTab(fragmentFiles[i], header=FALSE)
+					colnames(fragGr) <- c("chrom", "chromStart", "chromEnd", "barcode", "duplicateCount")
+					fragGr <- df2granges(fragGr, chrom.col=1L, start.col=2L, end.col=3L, strand.col=NULL, coord.format="B1RI", assembly=obj@genome, doSort=TRUE, adjNumChromNames=TRUE)
+					fragGr <- fragGr[elementMetadata(fragGr)[,"barcode"] %in% names(barcode2cellId)] # only take into account fragments that can be mapped to cells
+					if (length(fragGr) < 2) logger.error("Too few fragments corresponding to actual cells")
+					fragGrl <- split(fragGr, elementMetadata(fragGr)[,"barcode"])
+					names(fragGrl) <- barcode2cellId[names(fragGrl)]
+				logger.completed()
+
+				logger.start("Preparing insertion data")
+					insGrl <- lapply(fragGrl, getInsertionSitesFromFragmentGr)
+				logger.completed()
+				logger.start("Summarizing count data")
+					obj <- addCountDataFromGRL(obj, insGrl)
+				logger.completed()
+				
+				if (keepInsertionInfo) {
+					chunkedFragmentFiles <- obj@diskDump.fragments && .hasSlot(obj, "diskDump.fragments.nSamplesPerFile") && obj@diskDump.fragments.nSamplesPerFile > 1
+					logger.start("Adding fragment data to data structure")
+						if (chunkedFragmentFiles){
+							nCells <- length(fragGrl)
+							chunkL <- split(1:nCells, rep(1:ceiling(nCells/obj@diskDump.fragments.nSamplesPerFile), each=obj@diskDump.fragments.nSamplesPerFile)[1:nCells])
+							names(chunkL) <- NULL
+							for (k in 1:length(chunkL)){
+								iis <- chunkL[[k]]
+								cids <- names(fragGrl)[iis]
+								fn <- tempfile(pattern="fragments_", tmpdir=tempdir(), fileext = ".rds")
+								saveRDS(fragGrl[iis], fn)
+								obj@fragments[cids] <- rep(list(fn), length(iis))
+							}
+						} else {
+							for (cid in names(fragGrl)){
+								fgr <- fragGrl[[cid]]
+								if (obj@diskDump.fragments){
+									fn <- tempfile(pattern="fragments_", tmpdir=tempdir(), fileext = ".rds")
+									saveRDS(fgr, fn)
+									fgr <- fn
+								}
+								obj@fragments[[cid]] <- fgr
+							}
+						}
+					logger.completed()
+				}
+				
+			logger.completed()
+		}
+		# just to make sure: reorder fragment list
+		if (keepInsertionInfo) {
+			obj@fragments <- obj@fragments[getSamples(obj)]
+		}
+	logger.completed()
+	return(obj)
+}
+
+#-------------------------------------------------------------------------------
 #' DsATAC.cellranger
 #' 
 #' Create a DsATAC dataset from multiple input files output by 10x cellranger
@@ -24,7 +161,6 @@ DsATAC.cellranger <- function(sampleAnnot, sampleDirPrefixCol, genome, dataDir="
 
 	sampleIds <- as.character(sampleAnnot[,sampleIdCol])
 	rownames(sampleAnnot) <- sampleIds
-
 
 	if (nchar(dataDir) > 0){
 		sampleDirs <- file.path(dataDir, paste0(sampleAnnot[,sampleDirPrefixCol]))
@@ -132,78 +268,8 @@ DsATAC.cellranger <- function(sampleAnnot, sampleDirPrefixCol, genome, dataDir="
 		logger.completed()
 	}
 
-	logger.start("Creating DsATAC object")
-		obj <- DsATACsc(cellAnnot, genome, diskDump=diskDump, diskDump.fragments=keepInsertionInfo, sparseCounts=TRUE)
-		for (rt in names(regionSets)){
-			logger.info(c("Including region set:", rt))
-			obj <- regionAggregation(obj, regionSets[[rt]], rt, signal=NULL, dropEmpty=FALSE)
-		}
-		if (obj@diskDump.fragments && .hasSlot(obj, "diskDump.fragments.nSamplesPerFile")){
-			obj@diskDump.fragments.nSamplesPerFile <- 500L
-		}
-	logger.completed()
-
-	logger.start("Reading scATAC data (fragment data)")
-		nSamples <- length(sampleIds)
-		for (i in seq_along(sampleDirs)){
-			sid <- names(sampleDirs)[i]
-			logger.start(c("Importing sample", ":", sid, paste0("(", i, " of ", nSamples, ")")))
-				sampleIdx <- cellAnnot[,sampleIdCol]==sid
-				barcode2cellId <- cellAnnot[sampleIdx,"cellId"]
-				names(barcode2cellId) <- cellAnnot[sampleIdx,".CR.cellQC.barcode"]
-
-				logger.start("Preparing fragment data")
-					fragGr <- readTab(file.path(sampleDirs[i], "fragments.tsv.gz"), header=FALSE)
-					colnames(fragGr) <- c("chrom", "chromStart", "chromEnd", "barcode", "duplicateCount")
-					fragGr <- df2granges(fragGr, chrom.col=1L, start.col=2L, end.col=3L, strand.col=NULL, coord.format="B1RI", assembly=obj@genome, doSort=TRUE, adjNumChromNames=TRUE)
-					fragGr <- fragGr[elementMetadata(fragGr)[,"barcode"] %in% names(barcode2cellId)] # only take into account fragments that can be mapped to cells
-					if (length(fragGr) < 2) logger.error("Too few fragments corresponding to actual cells")
-					fragGrl <- split(fragGr, elementMetadata(fragGr)[,"barcode"])
-					names(fragGrl) <- barcode2cellId[names(fragGrl)]
-				logger.completed()
-
-				logger.start("Preparing insertion data")
-					insGrl <- lapply(fragGrl, getInsertionSitesFromFragmentGr)
-				logger.completed()
-				logger.start("Summarizing count data")
-					obj <- addCountDataFromGRL(obj, insGrl)
-				logger.completed()
-				
-				if (keepInsertionInfo) {
-					chunkedFragmentFiles <- obj@diskDump.fragments && .hasSlot(obj, "diskDump.fragments.nSamplesPerFile") && obj@diskDump.fragments.nSamplesPerFile > 1
-					logger.start("Adding fragment data to data structure")
-						if (chunkedFragmentFiles){
-							nCells <- length(fragGrl)
-							chunkL <- split(1:nCells, rep(1:ceiling(nCells/obj@diskDump.fragments.nSamplesPerFile), each=obj@diskDump.fragments.nSamplesPerFile)[1:nCells])
-							names(chunkL) <- NULL
-							for (k in 1:length(chunkL)){
-								iis <- chunkL[[k]]
-								cids <- names(fragGrl)[iis]
-								fn <- tempfile(pattern="fragments_", tmpdir=tempdir(), fileext = ".rds")
-								saveRDS(fragGrl[iis], fn)
-								obj@fragments[cids] <- rep(list(fn), length(iis))
-							}
-						} else {
-							for (cid in names(fragGrl)){
-								fgr <- fragGrl[[cid]]
-								if (obj@diskDump.fragments){
-									fn <- tempfile(pattern="fragments_", tmpdir=tempdir(), fileext = ".rds")
-									saveRDS(fgr, fn)
-									fgr <- fn
-								}
-								obj@fragments[[cid]] <- fgr
-							}
-						}
-					logger.completed()
-				}
-				
-			logger.completed()
-		}
-		# just to make sure: reorder fragment list
-		if (keepInsertionInfo) {
-			obj@fragments <- obj@fragments[getSamples(obj)]
-		}
-	logger.completed()
+	fragmentFiles <- file.path(sampleDirs, "fragments.tsv.gz")
+	obj <- DsATACsc.fragments(sampleAnnot, fragmentFiles, genome=genome, regionSets=regionSets, sampleIdCol=sampleIdCol, cellAnnot=cellAnnot, diskDump=diskDump, keepInsertionInfo=keepInsertionInfo)
 	
 	return(obj)
 }
