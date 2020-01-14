e7915ecf34e09e50dc56a823b5fbf4df16e183f1
Fabian Müller <fmueller@mpi-inf.mpg.de>
2020-01-14 14:19:56 -0800

    …

--- R/report_atac_normalization.R
+++ R/report_atac_normalization.R
@@ -125,8 +125,6 @@ setMethod("createReport_normalization",
 				i <- i + 1
 			}
 
-			print("blubb2")
-
 			# Side-by-side quantile heatmaps
 			cs <- getConfigElement("colorSchemesCont")[[".default"]]
 			colScheme <- circlize::colorRamp2(seq(0, maxCount, length.out=length(cs)), cs)
