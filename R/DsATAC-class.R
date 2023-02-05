7236d7e2a5d42d1013f72d5a702b9b4dd1f4ccc9
İrem Gündüz <irembgunduz@gmail.com>
2023-02-06 00:04:20 +0100

    fix bug with differential

--- R/DsATAC-class.R
+++ R/DsATAC-class.R
@@ -2889,7 +2889,7 @@ setMethod("getDESeq2Dataset",
 		}
 		#remove columns from the design that do not have replicates
 		idx <- sapply(designCols, FUN=function(cc){
-			is.numeric(ph[,cc]) || all(table(ph[,cc]) > 1 | table(ph[,cc]) == 0)
+			is.numeric(ph[,cc]) || all(table(ph[,cc]) > 1 | table(ph[,cc]) != 0)
 		})
 		if (sum(idx) < length(designCols)){
 			logger.warning(c("The following design columns will not be considered because they do not have replicates:", paste(designCols[!idx], collapse=",")))
